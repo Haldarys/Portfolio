3c2a0f8abb88dccd76313e8a459b5e17f65aea33
Haldarys <jp45230@gmail.com>
2020-02-23 02:07:27 +0100

    Fix display:none value
    The back face is now properly hidden
    Test code all in comments

--- anim_cube.js
+++ anim_cube.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
   var cube = $('.cube');
-  var val_rotateY = 0;
+  /*var val_rotateY = 0;
   var val_translateX = 0;
   var val_translateZ = 0;
 
-  var upZ = false;
+  var upZ = false;*/
 
   var faces = ['front','right','back','left'];
   var face_value = [[0,0,0], [90,-60,-60], [180,-120,0], [270,-60,60]] // [rY, tZ, tX]
@@ -13,20 +13,19 @@ $(document).ready(function(){
 
   $('.cube').click(function(){
     console.log("Click button front")
-    if(face_shown < 3){
-      face_shown += 1;
-    } else {
-      face_shown = 0;
-    }
+    $('.' + faces[face_shown+2<4 ? face_shown+2 : face_shown-2]).toggle(); //Unhide the back face
+    if(face_shown < 3){face_shown += 1;}
+    else {face_shown = 0;}
+    $('.' + faces[face_shown+2<4 ? face_shown+2 : face_shown-2]).toggle(); //Hide the new back face
 
     /*val_rotateY += 90;
     val_translateX -= 60;
     val_translateZ += upZ ? 60 : -60;
     upZ = !upZ;*/
-    replace_cube(val_rotateY, val_translateX, val_translateZ);
+    replace_cube(/*val_rotateY, val_translateX, val_translateZ*/);
   });
 
-  function replace_cube(rY, tZ, tX){
+  function replace_cube(/*rY, tZ, tX*/){
     //cube.css('transform','rotateY(' + val_rotateY + 'deg) translateZ(' + tZ + 'vmin) translateX(' + tX + 'vmin)');
     cube.css('transform','rotateY(' + face_value[face_shown][0] + 'deg) translateZ(' + face_value[face_shown][1] + 'vmin) translateX(' + face_value[face_shown][2] + 'vmin)');
   }
